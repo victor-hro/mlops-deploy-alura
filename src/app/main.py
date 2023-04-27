ccddeb2b8f3e37ee2fc6431bbc7ea94b107d2661
victor <victoroliveira.eng@hotmail.com>
2023-04-26 22:30:13 -0300

    att

--- src/app/main.py
+++ src/app/main.py
@@ -1,16 +1,17 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from flask_basicauth import BasicAuth
 from textblob import TextBlob
 import pandas as pd
 import pickle
 from sklearn.linear_model import LinearRegression
 import os
+import streamlit as st
 
 import dotenv
 project_dir = os.path.join(os.path.dirname(__file__), os.pardir)
 dotenv_path = os.path.join(project_dir.split('src')[0], 'venv/.env')
 
-model = pickle.load(open('../../models/model_houses.sav', 'rb'))
+model = pickle.load(open('models/model_houses.sav', 'rb'))
 
 app = Flask(__name__)
 app.config['BASIC_AUTH_USERNAME'] = os.getenv('BASIC_AUTH_USERNAME')
@@ -21,7 +22,8 @@ basic_auth = BasicAuth(app)
 # definindo rota base. Home = '/'
 @app.route('/')
 def home():
-    return "API"
+    # return "API"
+    return render_template('index.html')
 
 
 @app.route('/sentiment/<phrase>')
@@ -40,4 +42,5 @@ def cotacao():
     predict = model.predict([input_data])
     return jsonify(preco=predict[0])
 
-app.run(debug=True, host='0.0.0.0')
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0')
